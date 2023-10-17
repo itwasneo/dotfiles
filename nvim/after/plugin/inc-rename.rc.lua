076a3d3973dc1971697323c282b38aff2fb60334
itwasneo <kaan.yildiz@protonmail.com>
2023-10-17 21:26:03 +0300

    fix deleted files

--- nvim/after/plugin/inc-rename.rc.lua
+++
@@ -1,5 +0,0 @@
-local status, inc_rename = pcall(require, "inc_rename")
-
-if (not status) then return end
-
-inc_rename.setup()
--- nvim/lua/highlights.lua
+++
@@ -1,5 +0,0 @@
-vim.opt.termguicolors = true
--- vim.opt.winblend = 15
-vim.opt.wildoptions = "pum"
-vim.opt.pumblend = 15
---vim.opt.background = 'dark'
